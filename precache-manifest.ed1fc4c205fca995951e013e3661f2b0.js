594723b03a1076022faba949a509ebf620ae2d82
gfaktor97 <gfaktor_97@yahoo.com>
2020-04-27 10:09:58 +0100

    Updates

---
+++ precache-manifest.ed1fc4c205fca995951e013e3661f2b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff45cf112accbcb9e67d245e0111178",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cb245218df9baa766d25",
+    "url": "/monsters-rolodex/static/css/main.6e8a75c2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb245218df9baa766d25",
+    "url": "/monsters-rolodex/static/js/main.0a88e915.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
